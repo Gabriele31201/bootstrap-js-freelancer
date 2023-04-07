3d5d8c43acf8ee954ab959845621d8044a68551f
Gabriele31201 <gabri31201@gmail.com>
2023-04-07 12:11:53 +0200

    add price of type work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 let disocuntCodes = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
+
+
+function calculateServicePrice(event){
+    event.preventDefault();
+
+
+    let selectedTypeWork = document.getElementById("work-type").value;
+    console.log(selectedTypeWork);
+
+    switch(selectedTypeWork){
+        case "backend":
+          workPrice = 20.50
+                break;
+        case "frontend":
+            price += 15.30;
+                break;
+        case "projectanalyst":
+            price += 33.60;
+                break;
+    }
+
+}
